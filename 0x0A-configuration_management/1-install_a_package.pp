1bc5b6fe770cf33c8fe0a1f52098ba9c28846623
D-Tr3x <owosenidaniel13@gmail.com>
2025-01-22 19:30:56 +0100

    Checker's validation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,6 +6,5 @@ package { 'python3-pip':
 
 exec { 'install-flask':
   command     => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
-  refreshonly => true,
   require     => Package['python3-pip'],
 }
