dd5a3c7c37311fa2e352d41b45f535fe6c8027c9
D-Tr3x <owosenidaniel13@gmail.com>
2025-01-31 02:14:38 +0100

    Final update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,29 @@
 # Install and configure Nginx
 
+exec { 'dist update':
+  command  => '/usr/bin/apt-get update',
+  provider => 'shell',
+}
+
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['dist update'],
+}
+
+
+file {'/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+exec { 'redirect_me':
+  command => 'sed -i "24i\rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+  require => Package['nginx']
 }
 
 service { 'nginx':
   ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  source  => 'puppet:///modules/nginx/default.conf',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
   require => Package['nginx'],
 }
