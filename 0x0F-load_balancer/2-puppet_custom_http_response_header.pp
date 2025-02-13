81237a660df94dddba35204d438e3d377fd6a2c6
D-Tr3x <owosenidaniel13@gmail.com>
2025-02-13 03:58:32 +0100

    Updated 2-puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # Configures Nginx using `puppet` such that:
-# the name of the custom HTTP header is `X-Served-By`
-# the value of the custom HTTP header is the hostname
+# - The name of the custom HTTP header is `X-Served-By`
+# - The value of the custom HTTP header is the hostname
 
 exec { 'dist update':
   command  => '/usr/bin/apt-get update',
@@ -14,21 +14,42 @@ package { 'nginx':
 
 file { '/var/www/html/index.html':
   ensure  => file,
-  mode    => '0644',
   content => 'Hello World!',
   require => Package['nginx'],
 }
 
-exec { 'redirect_me':
-  command => 'sed -i "24i\\ 	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-  require  => Package['nginx'],
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => 'Not Found!',
+  require => Package['nginx'],
 }
 
-exec { 'custom_http_header':
-  command  => "sed -i \"25i\\    add_header X-Served-By \\\"\\\$(hostname)\\\";\" /etc/nginx/sites-available/default",
-  provider => 'shell',
-  require  => Package['nginx'],
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => @(END)
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    add_header X-Served-By "${::hostname}";
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+        internal;
+    }
+}
+| END
+  ,
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+
 }
 
 service { 'nginx':
