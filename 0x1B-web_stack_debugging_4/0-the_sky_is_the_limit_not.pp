47d3e25eda63c24f17749f44d786624c228e504a
D-Tr3x <owosenidaniel13@gmail.com>
2025-03-09 02:25:10 +0100

    Updated Fixes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,20 @@
 # Puppet script to modify Nginx's configuration
 
 
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
-  notify  => Exec['restart-nginx']
-}
+exec { 'fix-for-nginx':
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
+  path    => ['/usr/local/bin', '/bin'],
+  unless  => 'grep -q "4096" /etc/default/nginx'
+} ->
 
-exec { 'restart-nginx':
-  command     => '/bin/systemctl restart nginx',
-  path        => ['/bin', '/usr/bin', '/usr/sbin'],
+exec { 'nginx-restart':
+  command => 'service nginx restart',
+  path => ['/etc/init.d'],
   refreshonly => true
 }
+
+# exec { 'restart-nginx':
+#  command     => '/bin/systemctl restart nginx',
+#  path        => ['/bin', '/usr/bin', '/usr/sbin'],
+#  refreshonly => true
+# }
