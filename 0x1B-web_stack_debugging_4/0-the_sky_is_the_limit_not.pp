c23b7862413c13cbdfd6e3e71d119c3d8eb4ec7c
D-Tr3x <owosenidaniel13@gmail.com>
2025-03-09 02:37:29 +0100

    Updated script to pass puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,16 +5,10 @@ exec { 'fix-for-nginx':
   command => 'sed -i "s/15/4096" /etc/default/nginx',
   path    => ['/usr/local/bin', '/bin'],
   unless  => 'grep -q "4096" /etc/default/nginx'
-} ->
-
+}
+->
 exec { 'nginx-restart':
   command     => 'service nginx restart',
   path        => ['/etc/init.d'],
   refreshonly => true
 }
-
-# exec { 'restart-nginx':
-#  command     => '/bin/systemctl restart nginx',
-#  path        => ['/bin', '/usr/bin', '/usr/sbin'],
-#  refreshonly => true
-# }
